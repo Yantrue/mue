827f9da9ae3553210d1777320843bdfa87e3e80a
Yantrue <riyansanjaya703@gmail.com>
2025-07-29 09:37:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let slider = document.getElementById('slider');
+let slides = document.querySelectorAll('.slide');
+let index = 0;
+
+function nextSlide() {
+  index = (index + 1) % slides.length;
+  slider.style.transform = `translateX(-${index * 100}vw)`;
+}
+
+// Auto slide every 4 seconds
+setInterval(nextSlide, 4000);
 
